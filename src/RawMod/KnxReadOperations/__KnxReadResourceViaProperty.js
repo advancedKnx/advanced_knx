9fc0af5f6d5f5c766e1f852a3ac584c9aaac5a2d
knx_code@protonmail.com <knx_code@protonmail.com>
2019-08-11 15:57:18 +0200

    remove console.log statements

--- src/RawMod/KnxReadOperations/__KnxReadResourceViaProperty.js
+++ src/RawMod/KnxReadOperations/__KnxReadResourceViaProperty.js
@@ -3,14 +3,11 @@
 import KnxReadPropertyValue from './KnxReadPropertyValue'
 
 export default async (target, source, deviceResourceInformation, recvTimeout, conContext, errContext) => {
-// TODO remove debugging logs
   // Get the correct address and length
   const objectIndex = deviceResourceInformation.interfaceObjectRef || 0
   const startIndex = 1 // by default
   const propertyID = deviceResourceInformation.propertyID
   const elementCount = deviceResourceInformation.length // elementCount = Bytes = Length
-
-  console.log('[DEBUG] Reading %d elements from ID %d of object %d from index %d', elementCount, propertyID, objectIndex, startIndex)
 
   // Send the request and return the result
   return KnxReadPropertyValue.readPropertyValue(target, source, objectIndex, startIndex,
