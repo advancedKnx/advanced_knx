cba1295b3a4aad25476e0e597af8642e0158da12
knx_code@protonmail.com <knx_code@protonmail.com>
2019-08-25 14:46:12 +0200

    adapt to device resource system

--- src/RawMod/KnxReadOperations/KnxReadProgmodeStatus.js
+++ src/RawMod/KnxReadOperations/KnxReadProgmodeStatus.js
@@ -2,12 +2,12 @@
  * This file contains a function to read the progmode status of an KNX device *
  ******************************************************************************/
 
-import KnxReadDevMem from './KnxReadDevMem'
 import KnxConstants from '../../KnxConstants'
+import KnxReadDeviceResource from './KnxReadDeviceResource'
 
 export default {
   /*
-   * Function: KnxGetProgmodeStatus.getProgmodeStatus()
+   * Function: KnxGetProgmodeStatus.readProgmodeStatus()
    *
    *      This function reads the programming mode status of KNX devices
    *      It uses the KnxReadDevMem.readDevMem() function and does not introduce any own errors
@@ -21,6 +21,12 @@ export default {
    *                      Recommended to be around 2000 and should be raised to higher values when errors with the following
    *                      errorIDs are occurring:
    *                        65546 ('The target failed to respond!')
+   *                      Type: Integer
+   *
+   *      recvTimeout     Specifies how long to wait for an acknowledge message from the KNX device (in milliseconds)
+   *                      Recommended to be around 2000 and should be raised to higher values when errors with the following
+   *                      errorIDs are occurring:
+   *                        65542 ('The target failed to respond!')
    *                      Type: Integer
    *
    *      conContext      The KNX connection context - needed to operate with and on the connection to the KNX-IP interface
@@ -55,28 +61,25 @@ export default {
    *
    *      There may be other errors not labeled by RawMod (throw by the socket API when sending messages)
    */
-  getProgmodeStatus: async (target, recvTimeout, conContext, errContext) => {
+  readProgmodeStatus: async (target, recvTimeout, maskVersion, conContext, errContext) => {
     return new Promise(async resolve => {
-      let retVal = {
-        data: null,
-        error: 0
-      }
+      // read the resource
+      const val = await KnxReadDeviceResource.readDeviceResource(target, null, maskVersion,
+        'ProgrammingMode', KnxConstants.RESOURCE_ACCESS_TYPES.ALL, recvTimeout, conContext, errContext)
 
-      /*
-       * Pass the request to KnxReadDevMem.readDevMem()
-       *      source (conContext.options.phsyAddr) is the KNX-device address of the KNX-IP interface
-       *      address ([0x00, 0x60] == KnxConstants.KNX_MEMORY_ADDRS.MEMORY_PROGMODE_ADDR) is the address of the position of the programming-mode flags on all KXN-devices
-       *      length (1) is the amount of bytes to read from the address
-       */
-      let val = await KnxReadDevMem.readDevMem(target, conContext.options.physAddr, KnxConstants.KNX_MEMORY_ADDRS.MEMORY_PROGMODE_ADDR, 1, recvTimeout, conContext, errContext)
-
-      // Copy the data into 'retVal' and cut off the first three bytes (the byte count and the memory address)
-      if (!(retVal.error = val.error)) {
-        retVal.data = val.data.slice(3)
+      if (!val.error) {
+        // Cut of the first n bytes - n depends on the access way used
+        if (val.usedAccessMethod === KnxConstants.RESOURCE_ACCESS_TYPES.MEMORY) {
+          // Used memory address - cut off first three bytes
+          val.progmodeStatus = val.data.slice(3)
+        } else {
+          // Used property access - cut off first four bytes
+          val.progmodeStatus = val.data.slice(4)
+        }
       }
 
       // Return the result
-      resolve(retVal)
+      resolve(val)
     })
   }
 }
