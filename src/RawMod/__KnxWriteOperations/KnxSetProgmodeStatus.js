0525f8f8196b299b025323b8447fd38b2b295800
knx_code@protonmail.com <knx_code@protonmail.com>
2019-10-20 23:43:55 +0200

    fix comment

--- src/RawMod/__KnxWriteOperations/KnxSetProgmodeStatus.js
+++ src/RawMod/__KnxWriteOperations/KnxSetProgmodeStatus.js
@@ -50,10 +50,13 @@ export default {
    *      RawModErrors.UNDEF_ARGS - At least one argument is undefined
    *      RawModErrors.INVALID_ARGTYPES - At least one arguments has an invalid type
    *      RawModErrors.INVALID_DATALEN - The data argument has an invalid length
-   *      RawModErrors.TARGET_NOTACK- The target device did actively not acknowledge
+   *      RawModErrors.TARGET_NOTACK - The target device did actively not acknowledge
    *      RawModErrors.TIMEOUT_REACHED - The target device failed to respond in recvTimeout ms
    *      RawModErrors.INVALID_TARGET - target isn't a valid KNX address
    *      RawModErrors.INVALID_SOURCE - source is defined and it isn't a valid KNX address
+   *      RawModErrors.INVALID_MV_RN - The maskversion or resource name is invalid
+   *      RawModErrors.NO_WRITE_WAY_FOUND - No way to write the resource value found
+   *      RawModErrors.NO_WRITE_WAY_MATCHED - No way to write the resource value matches the criteria
    *
    *      There may be other errors not labeled by RawMod (throw by the socket API when sending messages)
    */
